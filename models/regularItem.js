a02d008708c986c031ebb57cd1afd45fe53ee015
Ken Lin <kel117@pitt.edu>
2020-08-12 21:13:05 -0400

    added regular item model

---
+++ models/regularItem.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const regularSchema = new Schema({
+  id: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  prices: [
+    {
+      type: String,
+      required: true,
+    },
+  ],
+  spicy: {
+    type: Boolean,
+    default: false,
+    required: true,
+  },
+  description: {
+    type: String,
+    default: "",
+  },
+});
+
+module.exports = mongoose.model("RegularItem", regularSchema);
