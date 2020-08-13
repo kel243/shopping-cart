7171af25b8327929b58ead5565ede00cc0a62fe2
Ken Lin <kel117@pitt.edu>
2020-08-12 22:08:55 -0400

    added order routes

---
+++ routes/orderRoutes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const router = express.Router();
+
+const orderController = require("../controllers/orderController");
+
+router.post("/", orderController.postOrder);
+
+module.exports = router;
