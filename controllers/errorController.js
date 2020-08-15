c1d77f031fac0f1a2186ba91e1478c77b1ef7236
Ken Lin <kel117@pitt.edu>
2020-08-14 21:08:30 -0400

    added error controller

---
+++ controllers/errorController.js
@@ -0,0 +1,76 @@
+const AppError = require("../utils/appError");
+
+// Handles cast errors
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
+// Handles validation errors
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+  const message = `Invalid input data. ${errors.join(". ")}`;
+  return new AppError(message, 400);
+};
+
+// Handles JWT errors
+const handleJWTError = (err) =>
+  new AppError("Invalid token. Please log in again!", 401);
+
+// Handles JWT expired errors
+const handleJWTExpiredError = (err) =>
+  new AppError("Your token has expired! Please log in again!", 401);
+
+const sendErrorDev = (err, req, res) => {
+  if (req.originalUrl.startsWith("/api")) {
+    return res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+      error: err,
+      stack: err.stack,
+    });
+  }
+  return res.status(err.statusCode).render("error", {
+    title: "Something went wrong!",
+    msg: err.message,
+  });
+};
+
+const sendErrorProd = (err, req, res) => {
+  // A) API
+  if (req.originalUrl.startsWith("/api")) {
+    // A) Operational, trusted error: send message to client
+    if (err.isOperational) {
+      return res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message,
+      });
+    }
+    // B) Programming or other unknown error: don't leak error details
+    return res.status(500).json({
+      status: "error",
+      message: "Something went very wrong!",
+    });
+  }
+
+  // B) RENDERED WEBSITE
+  // A) Operational, trusted error: send message to client
+  if (err.isOperational) {
+    return res.status(err.statusCode).render("error", {
+      title: "Something went wrong!",
+      msg: err.message,
+    });
+  }
+  // B) Programming or other unknown error: don't leak error details
+  return res.status(err.statusCode).render("error", {
+    title: "Something went wrong!",
+    msg: "Please try again later.",
+  });
+};
+
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  sendErrorDev(err, req, res);
+};
