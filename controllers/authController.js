f5111c3aa32dff27f37d00a9499967173b4a6ccc
Ken Lin <kel117@pitt.edu>
2020-08-14 21:08:42 -0400

    updated auth controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,3 @@
-const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { promisify } = require("util");
 const AppError = require("../utils/appError");
@@ -12,13 +11,14 @@ const signToken = (id) => {
   });
 };
 
-const createSendToken = (user, statusCode, res) => {
+const createSendToken = (user, statusCode, req, res) => {
   const token = signToken(user._id);
   const cookieOptions = {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
+    secure: req.secure || req.headers["x-forwarded-proto"] === "https",
   };
 
   if (process.env.NODE_ENV === "production") cookieOptions.secure = true;
@@ -27,39 +27,30 @@ const createSendToken = (user, statusCode, res) => {
 
   user.password = undefined;
 
-  res.status(statusCode).redirect("/admin");
+  console.log("fired");
+
+  res.status(statusCode).json({
+    status: "success",
+    token,
+    data: {
+      user,
+    },
+  });
 };
 
-exports.postLogin = (req, res, next) => {
-  const username = req.body.username;
-  const password = req.body.password;
+exports.postLogin = catchAsync(async (req, res, next) => {
+  const { username, password } = req.body;
 
-  User.findOne({ username: username })
-    .then((user) => {
-      if (!user) {
-        return res.status(422).render("login", {
-          errorMessage: "Invalid username or password!",
-        });
-      }
-      bcrypt
-        .compare(password, user.password)
-        .then((doMatch) => {
-          if (doMatch) {
-            createSendToken(user, 200, res);
-          } else {
-            console.log("failed");
-            res.status(422).render("login", {
-              errorMessage: "Invalid username or password!",
-            });
-          }
-        })
-        .catch((err) => {
-          console.log(err);
-          res.redirect("/login");
-        });
-    })
-    .catch((err) => console.log(err));
-};
+  // 2) Check if user exists && password is correct
+  const user = await User.findOne({ username }).select("+password");
+
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    return next(new AppError("Incorrect email or password", 401));
+  }
+
+  // 3) If everything ok, send token to client
+  createSendToken(user, 200, req, res);
+});
 
 exports.protect = catchAsync(async (req, res, next) => {
   // 1) Getting token and check if its there
@@ -98,3 +89,48 @@ exports.protect = catchAsync(async (req, res, next) => {
   res.locals.user = currentUser;
   next();
 });
+
+// Only for rendered pages, no errors!
+exports.isLoggedIn = async (req, res, next) => {
+  if (req.cookies.jwt) {
+    try {
+      // 1) verify token
+      const decoded = await promisify(jwt.verify)(
+        req.cookies.jwt,
+        process.env.JWT_SECRET
+      );
+
+      // 2) Check if user still exists
+      const currentUser = await User.findById(decoded.id);
+      if (!currentUser) {
+        return next();
+      }
+      // THERE IS A LOGGED IN USER
+      res.locals.user = currentUser;
+      return next();
+    } catch (err) {
+      return next();
+    }
+  }
+  next();
+};
+
+// Protect routes from logged in users (such as the login page)
+exports.alreadyLoggedIn = async (req, res, next) => {
+  try {
+    // 1) verifies token
+    const decoded = await promisify(jwt.verify)(
+      req.cookies.jwt,
+      process.env.JWT_SECRET
+    );
+
+    // 2) Check if user still exists
+    const currentUser = await User.findById(decoded.id);
+    if (currentUser) {
+      return next(new AppError("You are already logged in!", 401));
+    }
+  } catch (err) {
+    return next();
+  }
+  next();
+};
